04f22c4b51f0a219ca2a2dc0339c1ab3cb3315cf
Dan Croak <dan@statusok.com>
2024-09-19 18:30:36 -0400

    config: support more highlights

--- languages/haml/highlights.scm
+++ languages/haml/highlights.scm
@@ -1,9 +1,15 @@
-(tag) @tag
-(attribute) @attribute
-(ruby_block) @keyword
-(comment) @comment
-(string) @string
-(doctype) @keyword
-("%" @punctuation.delimiter)
 ("#" @punctuation.delimiter)
+("%" @punctuation.delimiter)
 ("." @punctuation.delimiter)
+(attribute) @attribute
+(class) @string
+(comment) @comment
+(doctype) @keyword
+(id) @string
+(name) @type
+(ruby_block) @keyword
+(ruby_block_output "=" @keyword)
+(ruby_block_run "-" @keyword)
+(string) @string
+(tag) @tag
+(text_content) @string
--- languages/haml/injections.scm
+++ languages/haml/injections.scm
@@ -1,5 +1,8 @@
 ((ruby_block) @content
  (#set! language ruby))
 
+((ruby_block_run) @content
+    (#set! language ruby))
+
 ((javascript_filter) @content
  (#set! language javascript))
